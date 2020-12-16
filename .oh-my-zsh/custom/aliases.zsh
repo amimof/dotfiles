2288721f833f5e1c4ec8340de73e5fd0e327f2a2
Amir Mofasser <amir.mofasser@gmail.com>
2020-12-16 10:37:50 +0100

    Add dotfiles for zsh and oh-my-zsh
    * Custom zsh file for aliases
    * Custom zsh file for environment variables
    * Dotfile .zshrc

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,12 @@
+# Re-attach to tmux session. Create a new session if none exists
+alias s='tmux attach || tmux new'
+
+# git repos home
+alias gh="cd ~/git && pwd"
+
+# separate kubeconfig for oc
+alias oc="oc --config=/Users/amir/.kube/ocp_config"
+
+# Alias kubectl to k and also add bash-completion for it
+alias k="kubectl"
+complete -F __start_kubectl k
