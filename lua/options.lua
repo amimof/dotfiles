69d21bf63df95773e4efdc0b23d291da06b25d9f
Amir Mofasser <amimof@users.noreply.github.com>
2025-01-02 21:03:47 +0100

    Adds Ghostty config

--- lua/options.lua
+++
@@ -1,9 +0,0 @@
-require "nvchad.options"
-
-local o = vim.o
-
--- Enable cursorline
-o.cursorlineopt = "both"
-
--- Decrease updatetime slightly
-o.updatetime = 300
--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,8 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
--- vim.cmd([[
---   :hi NeoTreeNormal guibg=#11121D
---   :hi NeoTreeNormalNC guibg=#11121D
---   :hi NeoTreeEndOfBuffer guibg=#11121D
--- ]])
+
+-- Disable spelling
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
